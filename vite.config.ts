3f4dfd245dc595f43a8f8ba75602023463b23583
Karthi2124 <karthiiyyappan2110@gmail.com>
2025-05-25 21:02:33 +0530

    Fix: use HashRouter and set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Health-buddy/",  // 👈 Required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
